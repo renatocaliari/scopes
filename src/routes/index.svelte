760622decde7794621af83abfeaf06f154095456
Cali (Renato Caliari) <renatoac82@gmail.com>
2022-05-01 11:29:24 -0300

    index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,10 +7,4 @@
 	import { goto } from '$app/navigation';
 </script>
 
-This template uses and has:
-<ul>
-	<li>DaisyUI & TailwindCSS</li>
-	<li>svelte-actions: https://github.com/sw-yx/svelte-actions</li>
-	<li>svelte-mount: https://github.com/ghostdevv/svelte-mount</li>
-	<li>uuid v4: to generate id</li>
-</ul>
+Here will be a landing page :)
