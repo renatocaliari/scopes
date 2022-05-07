80af7199d16490e7a9fef39a31506c51308d4cff
Cali (Renato Caliari) <renatoac82@gmail.com>
2022-05-07 10:31:20 -0300

    alert about a timeframe on dump screen

--- src/routes/scopes/dump.svelte
+++ src/routes/scopes/dump.svelte
@@ -76,6 +76,26 @@
 
 <NavigationScopes currentStep={0} let:currentStep>
 	<div slot="checkList">
+		<div class="alert alert-info text-white shadow-lg mb-4">
+			<div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="stroke-current flex-shrink-0 w-6 h-6"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+					/></svg
+				>
+				<span
+					>Thinking about a timeframe to execute a project is essential to know what to dump here,
+					make tradeoffs and sequence the execution of tasks on the other steps.</span
+				>
+			</div>
+		</div>
 		<NavigationCheckList {currentStep} {checkList} linkNextStep="/scopes/indispensable">
 			<div slot="buttons">
 				<label
