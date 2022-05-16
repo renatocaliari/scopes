70912125066f731ccc969a0706565a2ad1e7178d
Cali (Renato Caliari) <renatoac82@gmail.com>
2022-05-16 09:25:37 -0300

    change breadcrumb

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,12 +14,13 @@
 		<div class="hero-content text-center">
 			<div class="max-w-md">
 				<h1 class="text-6xl sm:text-8xl mb-2 font-bold">Scopefully</h1>
-				<span class="text-base sm:text-3xl">(beta v0.0.2)</span>
+				<span class="text-base sm:text-3xl">(beta v0.0.3)</span>
 				<p class="text-lg sm:text-2xl">
 					The tool to help you understand the execution of your next project before start it.
 				</p>
 				<div class="flex flex-col sm:flex-row items-center justify-center content-center gap-2">
-					<a href="/scopes/dump" sveltekit:prefetch class="btn btn-primary">Get Started for FREE</a>
+					<a href="/scopes/dump" sveltekit:prefetch class="btn btn-primary">Get Started fo r FREE</a
+					>
 					<a href="/about" sveltekit:prefetch class="hidden sm:flex btn btn-outline ml-4">About</a>
 				</div>
 				<p class="text-sm sm:text-lg">
